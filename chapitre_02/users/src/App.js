5660391fdbebacb1fa943585d1b014850187a342
anitamayousse <anita.mayousse@gmail.com>
2022-01-26 15:13:24 +0100

    2nd

--- chapitre_02/users/src/App.js
+++ chapitre_02/users/src/App.js
@@ -1,1 +1,17 @@
+import React from "react";
+import users from "./components/data/users.json";
+import "./App.css"
 
+class App extends React.Component  {
+  render (){
+  return (
+    <div className="App">
+      <h1>Users</h1>
+      {users.map((user) =>
+      (<p><b>Id:</b>{user.id} <b>Name:</b>{user.name} <b>Email:</b>{user.email} </p>
+        ))}
+    </div>
+  );
+}
+}
+export default App
