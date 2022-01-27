d32bc208f612acbaffa160c201463eab22d8f944
anitamayousse <anita.mayousse@gmail.com>
2022-01-27 10:45:16 +0100

    final commit

--- counter-users/src/components/UserInfo.js
+++ counter-users/src/components/UserInfo.js
@@ -5,8 +5,9 @@ class UserInfo extends React.Component {
     render()
     {
         return (
-
-    (<p><b>Id:</b>{this.props.id} <b>Name:</b>{this.props.name} <b>Email:</b>{this.props.email} </p>)
+    <div>
+        <p><b>Id:</b>{this.props.id} <b>Name:</b>{this.props.name} <b>Email:</b>{this.props.email} </p>
+    </div>
         )
 }
 }
